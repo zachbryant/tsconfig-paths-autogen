254b6306af1d1f57f2c15bc7b66dd9e4d68cc0e8
Zach Bryant <zdb282@gmail.com>
2021-06-12 02:35:49 -0700

    test project

---
+++ test/tsconfig.js
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const generatePaths = require('tsconfig-paths-autogen').default;
+const onmyjs = require('onmyjs').default;
+
+const baseUrl = 'src';
+
+module.exports = {
+	compilerOptions: {
+		target: 'es2020',
+		lib: ['dom', 'dom.iterable', 'esnext'],
+		allowJs: true,
+		skipLibCheck: true,
+		esModuleInterop: true,
+		allowSyntheticDefaultImports: true,
+		noImplicitReturns: true,
+		strict: true,
+		forceConsistentCasingInFileNames: true,
+		module: 'es2020',
+		moduleResolution: 'node',
+		resolveJsonModule: true,
+		isolatedModules: true,
+		noEmit: false,
+		jsx: 'react',
+		newLine: 'lf',
+		baseUrl,
+		paths: generatePaths(baseUrl, {
+			rootAlias: '@',
+			maxDirectoryDepth: 2,
+			excludeAliasForSubDirectories: ['components'],
+			includeAliasForDirectories: {
+				common: 'b/c/d',
+			},
+		}),
+	},
+	include: ['src'],
+	exclude: ['lib', 'build', 'node_modules', 'scripts', 'webpack', 'jest', 'tests', 'templates'],
+};
+
+onmyjs(module.exports, undefined, true);
